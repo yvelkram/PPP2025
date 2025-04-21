24cc332e194965ddd07ee8a668ca1e313b91a033
이벨크람(yvelkram) <sole0621@gmail.com>
2025-04-21 16:10:43 +0900

    Update 25-03-10.py

--- programs/25-03-10.py
+++ programs/25-03-10.py
@@ -4,32 +4,8 @@
 # break, continue
 
 
-def csv_read(filename):
-    lines = open(filename, encoding="UTF-8-sig").readlines()
-
-    result = {}
-    for line in lines[1:]:
-        token = line.replace("\n", "").replace(" ", "").split(",")
-        result[token[0]] = int(token[1]) / int(token[2])
-
-    return result
-
-
 def main():
-    # 0 : 작목 / 1 : 열량(kcal/회) / 2 : 총 내용량(g)
-    fruits = csv_read("../data/hw09/calorie_db.csv")
-
-    fruits_eat = {
-        "바나나" : 100,
-        "살구" : 100,
-        "사과" : 100
-    }
-
-    total_calorie = 0
-    for eat in fruits_eat.keys():
-        total_calorie += fruits[eat] * fruits_eat[eat]
-
-    print(total_calorie)
+    print("a")
 
 if __name__ == '__main__':
     main()
